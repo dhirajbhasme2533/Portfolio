0d1b831a02654c7986d78cbc855b067671b9a9ce
dhirajbhasme2533 <dhirajbhasmeuiux@gmail.com>
2025-01-07 12:36:47 +0530

    Spelling change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='text-xs md:text-xs w-full px-5 py-5 lg:px-20 lg:py-3 lg:text-lg font-sans text-white bg-[#2E282B] flex flex-col lg:flex-row justify-between items-center'>
-      <div>Dhiraj Bhamse</div>
+      <div>Dhiraj Bhasme</div>
       <div className='text-[#a4a4a4]'>&copy; Copyright 2024 - All Right Reserved</div>
     </div>
   )
